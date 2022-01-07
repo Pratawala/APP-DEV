caa2d3df2db4cbe5838bb87fbb4c2ab4131f6189
Prata Wala <Kingsleylow2013@gmail.com>
2022-01-07 16:20:22 +0800

    database

---
+++ App dev/Python/database.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app= Flask(__name__)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False #makes warning message go away
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///user_database' #url of database
+
+
+
+
+db= SQLAlchemy(app)
+
+class User(db.Model): #class will be mapped to a table
+    id= db.Column(db.Integer ,primary_key=True) #creating column (this is used by sql)
+
+    #Following columns is for storing data we want
+    name= db.column(db.String(50))  #Value is a string and 50 character long
+    password=db.Column(db.String(50))
+    # subscription_type=db.Column(db.String(1),default=1)
+
+
+
