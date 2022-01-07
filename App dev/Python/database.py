a13d5f1728feb3d25a79effa934284b30bd18be3
Prata Wala <Kingsleylow2013@gmail.com>
2022-01-07 19:05:50 +0800

    added database file

--- App dev/Python/database.py
+++ App dev/Python/database.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app= Flask(__name__)
 
@@ -8,16 +9,34 @@ app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///site.db' #url of database
 
 
 
-
-db= SQLAlchemy(app)
+db=SQLAlchemy(app)
 
 class User(db.Model): #class will be mapped to a table
     id= db.Column(db.Integer ,primary_key=True) #creating column (this is used by sql)
-
+    #primary key: Unique id for users
     #Following columns is for storing data we want
-    name= db.column(db.String(50))  #Value is a string and 50 character long
-    password=db.Column(db.String(50))
+    username= db.Column(db.String(50),unique=True,nullable=False) #Value is a string , 50 character long, unique and cannot be empty
+    email= db.Column(db.String(120),unique=True,nullable=False)
+    image_file =db.Column(db.String(20),nullable=False,default='default.jpg')
+    password =db.Column(db.String(60),nullable=False)
     # subscription_type=db.Column(db.String(1),default=1)
+    #post=db.relationship('Post',backref="author",lazy=True)
 
 
+
+
+    def __repr__(self):#what will be printed when the user is created
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
+
+
+# class Post(db.Model): #
+#     id=db.Column(db.Integer,primary_Key=True) 
+#     title=db.Column(db.String(100),nullable=False)
+#     date_posted= db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
+#     content= db.Column(db.Text,nullable=False)
+#     user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+
+
+    # def __repr__(self):#what will be printed when the user is created
+    #     return f"User('{self.title}','{self.date_posted}')"
 
