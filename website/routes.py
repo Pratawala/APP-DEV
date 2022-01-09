4e562bc56ecc3866cc42e2179689a62e5f68c4a8
Prata Wala <Kingsleylow2013@gmail.com>
2022-01-09 20:03:59 +0800

    some hashing bs its broken asf

--- website/routes.py
+++ website/routes.py
@@ -1,7 +1,9 @@
 from flask import render_template,flash,redirect,url_for,flash,redirect
+from wtforms.validators import Email
 from website.models import User
 from website.forms import RegistrationForm,LoginForm
-from website import app
+from website import app,db,bcrypt
+
 
 @app.route("/")
 def home():
@@ -9,16 +11,19 @@ def home():
 #do  not remove this apprently it crashes the entire server when removed
 
 
-@app.route("/")
-def test():
-    return render_template("null2.html")
+
 
 @app.route("/register",methods=['GET','POST'])
 def register():
     form=RegistrationForm()  #creates a form object from Registraion form
     if form.validate_on_submit():
+        hashed_password=bcrypt.generate_password_hash(form.password).decode('utf-8') #hash value will be in string instead of bytes
+        user=User(username=form.username.data,email=form.email.data,password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are not able to log in','success')
         flash(f'Account successfully created for {form.username.data}!') #flash displays a popup message
-        return redirect(url_for("home"))
+        return redirect(url_for("login"))
     return render_template("register.html",title="Register",form=form)
     #register.html is the file name for the register form
 
