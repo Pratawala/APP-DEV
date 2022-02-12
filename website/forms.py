2661374ce48a8cabf8be9407b2462c4f1a2382ff
lgf2111 <lgf2111@gmail.com>
2022-02-12 23:06:24 +0800

    implemented account page

--- website/forms.py
+++ website/forms.py
@@ -1,6 +1,7 @@
 
 from flask_wtf import FlaskForm
 from wtforms import  BooleanField, StringField, PasswordField,SubmitField
+from flask_wtf.file import FileField, FileAllowed
 from wtforms.validators import DataRequired ,Length,Email,EqualTo, ValidationError #imports  validators , what they validates is in their name  
 from website.models import User
 from flask_login import current_user
@@ -57,47 +58,65 @@ class LoginForm(FlaskForm):  #AREA FOR HTML-PY ERRORR
 
 
 
-class UpdateaccForm(FlaskForm):  #AREA FOR HTML-PY ERRORR
-    username= StringField("Username", validators=[DataRequired(),
-        Length(min=2,max=20)]) #Username will also be used as label in html 
+# class UpdateaccForm(FlaskForm):  #AREA FOR HTML-PY ERRORR
+#     username= StringField("Username", validators=[DataRequired(),
+#         Length(min=2,max=20)]) #Username will also be used as label in html 
 
-    email= StringField('Email',validators=[DataRequired(),Email()])
-    #insert code to update profile pic, will add in future
-    submit= SubmitField('Save Changes') #Python For the Submit python
+#     email= StringField('Email',validators=[DataRequired(),Email()])
+#     #insert code to update profile pic, will add in future
+#     submit= SubmitField('Save Changes') #Python For the Submit python
+
+#     def validate_username(self, username):
+#         if username.data !=current_user.username:
+#             user = User.query.filter_by(username=username.data).first()   #Checks if user is already in database
+#         if user: #if user is True this conditional will be activated
+#             raise ValidationError('Username is already taken!')
+ 
+#     def validate_email(self, email): 
+#         if email.data !=current_user.username:
+#             user = User.query.filter_by(email=email.data).first()   #Checks if user is already in database
+#         if user: #if user is none this conditional will be activated
+#             raise ValidationError('Email is already in use!')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
 
     def validate_username(self, username):
-        if username.data !=current_user.username:
-            user = User.query.filter_by(username=username.data).first()   #Checks if user is already in database
-        if user: #if user is True this conditional will be activated
-            raise ValidationError('Username is already taken!')
- 
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
+
+class request_reset(FlaskForm):
+    email=email= StringField('Email',validators=[DataRequired(),Email()])
+    submit =SubmitField('Request Password reset')
+
+
     def validate_email(self, email): 
         if email.data !=current_user.username:
             user = User.query.filter_by(email=email.data).first()   #Checks if user is already in database
-        if user: #if user is none this conditional will be activated
-            raise ValidationError('Email is already in use!')
+        if user is None: #if user is none this conditional will be activated
+            raise ValidationError('Email is not registered with website')
 
 
 
-    class request_reset(FlaskForm):
-        email=email= StringField('Email',validators=[DataRequired(),Email()])
-        submit =SubmitField('Request Password reset')
+class reset_password(FlaskForm):
+        password= PasswordField('Password',validators=[DataRequired()])
+confirm_password= PasswordField('Password',validators=[DataRequired(),EqualTo("password")]) 
 
+submit= SubmitField("Reset Password")
 
-        def validate_email(self, email): 
-            if email.data !=current_user.username:
-                user = User.query.filter_by(email=email.data).first()   #Checks if user is already in database
-            if user is None: #if user is none this conditional will be activated
-                raise ValidationError('Email is not registered with website')
-
-    
-    
-    class reset_password(FlaskForm):
-         password= PasswordField('Password',validators=[DataRequired()])
-    confirm_password= PasswordField('Password',validators=[DataRequired(),EqualTo("password")]) 
-
-    submit= SubmitField("Reset Password")
-    
 
     
 
